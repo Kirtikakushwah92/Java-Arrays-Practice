dd9211be327a4a9e02e79b1471c4409699b0eb04
Kirtika Kushwah <kirtikakushwah9294@gmail.com>
2026-06-09 23:19:02 +0530

    add gitignore and remove class file

--- src/java_arrays/Program_1.java
+++ src/java_arrays/Program_1.java
@@ -1,5 +1,22 @@
 package java_arrays;
-
+//Print biggest element , smallest elements and their difference from the given array.
 public class Program_1 {
-
+	public static void main(String[]args) {
+		int[] a = {1,2,3,4,5,6};
+		printBigAndSmallElement(a);
+	}	
+	public static void printBigAndSmallElement(int[] a) {
+		int big = a[0];
+		int small = a[0];
+		for(int x:a) {
+			if(x>big)
+				big = x;
+			if(x<small)
+				small = x;
+		}
+		System.out.println("Biggest Element : "+big);
+		System.out.println("Smallest Element : "+small);
+		System.out.println("Difference : "+(big-small));
+		
+	}
 }
