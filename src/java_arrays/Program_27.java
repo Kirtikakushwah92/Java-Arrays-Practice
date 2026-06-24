f52ea623ec2642ae328f643a5a1a34ec1d1a593c
Kirtika Kushwah <kirtikakushwah9294@gmail.com>
2026-06-24 23:31:27 +0530

    check arrray is sorted or not

--- src/java_arrays/Program_27.java
+++ src/java_arrays/Program_27.java
@@ -5,9 +5,25 @@ package java_arrays;
 //o/p: Array is sorted
 public class Program_27 {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+        int[] a = {2, 7, 7, 8, 9};
+        checkSorted(a);
+    }
 
-	}
+    public static void checkSorted(int[] a) {
+        boolean isSorted = true;
 
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
 }
