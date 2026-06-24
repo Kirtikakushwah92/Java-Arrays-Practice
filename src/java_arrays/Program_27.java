4791c6c5710f78ee17e39fd6e7a641c6b539d6ad
Kirtika Kushwah <kirtikakushwah9294@gmail.com>
2026-06-24 23:29:37 +0530

    add program 27

---
+++ src/java_arrays/Program_27.java
@@ -0,0 +1,13 @@
+package java_arrays;
+//WAJP to check whether a given array is in
+//sorted order or not.
+//i/p: [2, 7, 7, 8, 9]
+//o/p: Array is sorted
+public class Program_27 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
